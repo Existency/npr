b79a014a0e164f21f761166ff96a7378d1169dcc
Luis Pereira <36925029+LuisPereira23@users.noreply.github.com>
2022-04-22 15:25:10 +0100

    pygame menu

--- bomberdude/client/client.py
+++ bomberdude/client/client.py
@@ -1,41 +1,139 @@
-from .networking import NetClient
 import pygame
+import pygame_menu
 
 # pygame goes brr
 
 
 class Client:
-    def __init__(self, host, port):
-        self.net = NetClient(host, port)
+    
+    COLOR_BACKGROUND = (153, 153, 255)
+    COLOR_BLACK = (0, 0, 0)
+    COLOR_WHITE = (255, 255, 255)
+    FPS = 60.0
+    MENU_BACKGROUND_COLOR = (102, 102, 153)
+    MENU_TITLE_COLOR = (51, 51, 255)
+    INFO = None
+    TILE_SIZE = None
+    WINDOW_SIZE = None
+    surface = None
+    clock = None
+    #player_alg = Algorithm.PLAYER
+    #en1_alg = Algorithm.DIJKSTRA
+    #en2_alg = Algorithm.DFS
+    #en3_alg = Algorithm.DIJKSTRA
+    show_path = True
 
-    def run(self):
-        # init pygame
+    #def __init__(self, host, port):
+    #    net = NetClient(host, port)
+        
+        
+    def change_player(value, c):
+        global player_alg
+        player_alg = c
+    
+    def run_game(self):
+        print("run game")
+        #game.game_init( self.show_path, self.player_alg, self.en1_alg, self.en2_alg, self.en3_alg, self.TILE_SIZE)
+        
+    def main_background(self):
+        self.surface.fill(self.COLOR_BACKGROUND)
+            
+    def menu_loop(self):
+        pygame.display.init()
+        self.INFO = pygame.display.Info()
+        self.TILE_SIZE = int(self.INFO.current_h * 0.065)
+        self.WINDOW_SIZE = (10 * self.TILE_SIZE, 10 * self.TILE_SIZE)
+        self.surface = pygame.display.set_mode(self.WINDOW_SIZE)
+        
         pygame.init()
         self.screen = pygame.display.set_mode((640, 480))
         self.clock = pygame.time.Clock()
+        
+        pygame.init()
 
-        # main loop
+        pygame.display.set_caption('Bomberman')
+        clock = pygame.time.Clock()
+
+
+        play_menu = pygame_menu.Menu(
+            theme=pygame_menu.themes.THEME_DEFAULT,
+            height=int(self.WINDOW_SIZE[1] * 0.7),
+            width=int(self.WINDOW_SIZE[0] * 0.7),
+            onclose=pygame_menu.events.NONE,
+            title='Play menu'
+        )
+
+        play_options = pygame_menu.Menu(theme=pygame_menu.themes.THEME_DEFAULT,
+            height=int(self.WINDOW_SIZE[1] * 0.7),
+            width=int(self.WINDOW_SIZE[0] * 0.7),
+            title='Options'
+        )
+        
+        #play_options.add.selector("Character 1", [("Player", self.Algorithm.PLAYER), ("DFS", self.Algorithm.DFS),
+        #                                      ("DIJKSTRA", self.Algorithm.DIJKSTRA), ("None", self.Algorithm.NONE)], onchange=self.change_player)
+
+        play_options.add.button('Back', pygame_menu.events.BACK)
+        play_menu.add.button('Start',
+                            self.run_game)
+
+        play_menu.add.button('Options', play_options)
+        play_menu.add.button('Return  to  main  menu', pygame_menu.events.BACK)
+
+        about_menu_theme = pygame_menu.themes.Theme(
+            selection_color=self.COLOR_WHITE,
+            widget_font=pygame_menu.font.FONT_BEBAS,
+            title_font_size=self.TILE_SIZE,
+            title_font_color=self.COLOR_BLACK,
+            title_font=pygame_menu.font.FONT_BEBAS,
+            widget_font_color=self.COLOR_BLACK,
+            widget_font_size=int(self.TILE_SIZE*0.4),
+            background_color=self.MENU_BACKGROUND_COLOR,
+            title_background_color=self.MENU_TITLE_COLOR,
+        )
+        
+        about_menu = pygame_menu.Menu(theme=about_menu_theme,
+        height=int(self.WINDOW_SIZE[1] * 0.7),
+        width=int(self.WINDOW_SIZE[0] * 0.7),
+        onclose=pygame_menu.events.NONE,
+        title='About'
+        )
+        about_menu.add.label("Player_controls: ")
+        about_menu.add.label("Movement:_Arrows")
+        about_menu.add.label("Plant bomb:_Space")
+        about_menu.add.label("Sprite: ")
+
+        about_menu.add.label("https://opengameart.org/content")
+        about_menu.add.label("/bomb-party-the-complete-set")
+
+        main_menu = pygame_menu.Menu(
+            theme=pygame_menu.themes.THEME_DEFAULT,
+            height=int(self.WINDOW_SIZE[1] * 0.6),
+            width=int(self.WINDOW_SIZE[0] * 0.6),
+            onclose=pygame_menu.events.NONE,
+            title='Main menu'
+        )
+
+        main_menu.add.button('Play', play_menu)
+        main_menu.add.button('About', about_menu)
+        main_menu.add.button('Quit', pygame_menu.events.EXIT)
+        
         while True:
-            # handle events
-            for event in pygame.event.get():
+        
+            clock.tick(self.FPS)
+
+            self.main_background()
+
+            events = pygame.event.get()
+            for event in events:
                 if event.type == pygame.QUIT:
-                    self.net.terminate()
-                    return
-                elif event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_ESCAPE:
-                        self.net.terminate()
-                        return
+                    exit()
 
-            # update
-            # self.net.update()
+            main_menu.mainloop(self.surface, self.main_background, disable_loop=False, fps_limit=0)
+            main_menu.update(events)
+            main_menu.draw(self.surface)
 
-            # draw
-            self.screen.fill((0, 0, 0))
-            # self.net.draw(self.screen)
             pygame.display.flip()
 
-            # sleep
-            self.clock.tick(60)
 
     def stop(self):
         self.net.terminate()
