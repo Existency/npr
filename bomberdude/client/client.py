007e4e2a6db19db020743da8e4c6a6c159e4777b
Existency <existency@pm.me>
2022-06-05 15:47:14 +0100

    fix clt and client.py

--- bomberdude/client/client.py
+++ bomberdude/client/client.py
@@ -10,7 +10,7 @@ import time
 
 
 class Client:
-    
+
     COLOR_BACKGROUND = (153, 153, 255)
     COLOR_BLACK = (0, 0, 0)
     COLOR_WHITE = (255, 255, 255)
@@ -28,43 +28,42 @@ class Client:
     en3_alg = Algorithm.DIJKSTRA
     show_path = True
 
-    def __init__(self, args):
+    def __init__(self, args, node_path, node_ipv6):
         self.args = args
-        self.cli = NetClient((self.args.address, 8080), self.args.port)
-        
-        
-    def change_player(self,value, c):
+        self.cli = NetClient((self.args.address, 8080), self.args.port,
+                             node_path=node_path, byte_address=node_ipv6)
+
+    def change_player(self, value, c):
         #global player_alg
         self.player_alg = c
-    
-    def run_game(self):  
+
+    def run_game(self):
         print("run game")
         self.TILE_SIZE = int(self.INFO.current_h * 0.025)
-        game_init( self.show_path, self.player_alg, self.en1_alg, self.en2_alg, self.en3_alg, self.TILE_SIZE,self.cli,self.args)
-        
+        game_init(self.show_path, self.player_alg, self.en1_alg,
+                  self.en2_alg, self.en3_alg, self.TILE_SIZE, self.cli, self.args)
+
         self.TILE_SIZE = int(self.INFO.current_h * 0.065)
         self.WINDOW_SIZE = (13 * self.TILE_SIZE, 13 * self.TILE_SIZE)
-        
-        
+
     def main_background(self):
         self.surface.fill(self.COLOR_BACKGROUND)
-            
+
     def menu_loop(self):
         pygame.display.init()
         self.INFO = pygame.display.Info()
         self.TILE_SIZE = int(self.INFO.current_h * 0.065)
         self.WINDOW_SIZE = (10 * self.TILE_SIZE, 10 * self.TILE_SIZE)
         self.surface = pygame.display.set_mode(self.WINDOW_SIZE)
-        
+
         pygame.init()
         self.screen = pygame.display.set_mode((640, 480))
         self.clock = pygame.time.Clock()
-        
+
         pygame.init()
 
         pygame.display.set_caption('Bomberman')
         clock = pygame.time.Clock()
-
 
         play_menu = pygame_menu.Menu(
             theme=pygame_menu.themes.THEME_DEFAULT,
@@ -75,17 +74,17 @@ class Client:
         )
 
         play_options = pygame_menu.Menu(theme=pygame_menu.themes.THEME_DEFAULT,
-            height=int(self.WINDOW_SIZE[1] * 0.7),
-            width=int(self.WINDOW_SIZE[0] * 0.7),
-            title='Options'
-        )
-        
+                                        height=int(self.WINDOW_SIZE[1] * 0.7),
+                                        width=int(self.WINDOW_SIZE[0] * 0.7),
+                                        title='Options'
+                                        )
+
         play_options.add.selector("Character 1", [("Player", Algorithm.PLAYER), ("DFS", Algorithm.DFS),
-                                              ("DIJKSTRA", Algorithm.DIJKSTRA), ("None", Algorithm.NONE)], onchange=self.change_player)
+                                                  ("DIJKSTRA", Algorithm.DIJKSTRA), ("None", Algorithm.NONE)], onchange=self.change_player)
 
         play_options.add.button('Back', pygame_menu.events.BACK)
         play_menu.add.button('Start',
-                            self.run_game)
+                             self.run_game)
 
         play_menu.add.button('Options', play_options)
         play_menu.add.button('Return  to  main  menu', pygame_menu.events.BACK)
@@ -101,13 +100,13 @@ class Client:
             background_color=self.MENU_BACKGROUND_COLOR,
             title_background_color=self.MENU_TITLE_COLOR,
         )
-        
+
         about_menu = pygame_menu.Menu(theme=about_menu_theme,
-        height=int(self.WINDOW_SIZE[1] * 0.7),
-        width=int(self.WINDOW_SIZE[0] * 0.7),
-        onclose=pygame_menu.events.NONE,
-        title='About'
-        )
+                                      height=int(self.WINDOW_SIZE[1] * 0.7),
+                                      width=int(self.WINDOW_SIZE[0] * 0.7),
+                                      onclose=pygame_menu.events.NONE,
+                                      title='About'
+                                      )
         about_menu.add.label("Player_controls: ")
         about_menu.add.label("Movement:_Arrows")
         about_menu.add.label("Plant bomb:_Space")
@@ -127,9 +126,9 @@ class Client:
         main_menu.add.button('Play', play_menu)
         main_menu.add.button('About', about_menu)
         main_menu.add.button('Quit', pygame_menu.events.EXIT)
-        
+
         while True:
-        
+
             clock.tick(self.FPS)
 
             self.main_background()
@@ -139,16 +138,15 @@ class Client:
                 if event.type == pygame.QUIT:
                     exit()
 
-            main_menu.mainloop(self.surface, self.main_background, disable_loop=False, fps_limit=0)
+            main_menu.mainloop(self.surface, self.main_background,
+                               disable_loop=False, fps_limit=0)
             main_menu.update(events)
             main_menu.draw(self.surface)
 
             pygame.display.flip()
-            
-        
+
     def start_game(self):
         self.menu_loop()
-
 
     def stop(self):
         self.net.terminate()
--- bomberdude/clt.py
+++ bomberdude/clt.py
@@ -3,7 +3,7 @@
 import argparse
 from client.networking import NetClient
 from client.client import Client
-from common.core_utils import get_node_path
+from common.core_utils import get_node_ipv6, get_node_path
 from logging import INFO, DEBUG, ERROR, WARNING
 
 if __name__ == '__main__':
@@ -31,15 +31,20 @@ if __name__ == '__main__':
         log_lvl = INFO
 
     # get node's path
-   # node_path = get_node_path(args.node)
-   # if node_path is None:
-   #     print("Node's path not found")
-   #     exit(1)
+    node_path = get_node_path(args.node)
+    if node_path is None:
+        print("Node's path not found")
+        exit(1)
 
-    pymenu = Client(args)
+    node_ipv6 = get_node_ipv6(args.node)
+    if node_ipv6 is None:
+        print("Node's ipv6 not found")
+        exit(1)
+
+    pymenu = Client(args, node_path, node_ipv6)
     pymenu.start_game()
 
-    #cli = NetClient((args.address, 8080), args.port,
+    # cli = NetClient((args.address, 8080), args.port,
     #                 level=log_lvl, npath=node_path)
-    #cli.join_server(args.id)
-    #cli.start()
+    # cli.join_server(args.id)
+    # cli.start()
